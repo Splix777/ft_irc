7c648647d9c5210c7b18ef08f0da3d07be521fb1
Splix <federicosalalois@gmail.com>
2023-10-22 16:25:08 +0200

    added command def

--- srcs/cmds/Notice.cpp
+++ srcs/cmds/Notice.cpp
@@ -1,2 +1,30 @@
 #include "Notice.hpp"
 #include "IO.hpp"
+
+// 3.3.2 Notice
+
+//       Command: NOTICE
+//    Parameters: <msgtarget> <text>
+
+//    The NOTICE command is used similarly to PRIVMSG.  The difference
+//    between NOTICE and PRIVMSG is that automatic replies MUST NEVER be
+//    sent in response to a NOTICE message.  This rule applies to servers
+
+
+
+// Kalt                         Informational                     [Page 24]
+
+// RFC 2812          Internet Relay Chat: Client Protocol        April 2000
+
+
+//    too - they MUST NOT send any error reply back to the client on
+//    receipt of a notice.  The object of this rule is to avoid loops
+//    between clients automatically sending something in response to
+//    something it received.
+
+//    This command is available to services as well as users.
+
+//    This is typically used by services, and automatons (clients with
+//    either an AI or other interactive program controlling their actions).
+
+//    See PRIVMSG for more details on replies and examples.
