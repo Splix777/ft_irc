0efde720d5d7b23bd0f861fd97522eee24161e08
Splix <federicosalalois@gmail.com>
2023-10-26 17:42:31 +0200

    server2server

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -13,17 +13,15 @@ static void sigintHandler(int signum)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3 && (argc != 4 || (argc == 4 && strcmp(argv[3], "DEBUG") != 0)))
+	if (argc < 3 && argc > 5)
 	{
-		std::cout << BACK_RED << BLACK << "ERROR: ./ircserv [host:port_network:password_network] <port> <password>" << RESET << std::endl;
+		std::cerr << "ERROR: ./ircserv [host:port_network:password_network] <port> <password>" << std::endl;
 		return (1);
 	}
 
-	bool DEBUG = (argc == 4 && strcmp(argv[3], "DEBUG") == 0) ? TRUE : FALSE;
-	
 	signal(SIGINT, sigintHandler);
 
-	IO IO(argv[1], argv[2], DEBUG);
+	IO IO(argc, argv);
 	
 	while (g_status)
 	{
@@ -38,8 +36,11 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	std::cout << "Server is shutting down..." << std::endl;
-	IO.stop();
+	if (g_status == FALSE)
+    {
+		std::cout << "Server is shutting down..." << std::endl;
+        IO.stop();
+	}
 
 	return (0);
 }
